563a5085e4ac0bf91aa84fa7939ed0021a42e918
Javascript <doman.20042020@gmail.com>
2024-04-15 22:44:17 +0700

    Synchronous Function and Asynchronous Function

---
+++ #1.js
@@ -0,0 +1,25 @@
+//!#1 What's Async Javascript - Khái niệm BẤT ĐỒNG BỘ JS
+//Synchronous Function => hàm đồng bộ
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+
+//ở đoạn mã này chúng ta có thể thấy các log sẽ chạy theo thứ tự đầu đến cuối từ log1 rồi đến log2 v.v
+//Asynchronous Function => Hàm bất đồng bộ
+function delayedGreeting() {
+    console.log('Chào bạn!'); // Bước 1: In ra lời chào trước
+    setTimeout(() => {
+        console.log('Xin chào sau 2 giây!'); // Bước 3: In ra lời chào sau khi đợi 2 giây
+    }, 2000);
+}
+
+console.log('Bắt đầu chương trình.'); // Bước 2: In ra thông báo bắt đầu chương trình
+
+delayedGreeting(); // Gọi hàm delayedGreeting để in ra lời chào sau một khoảng thời gian
+
+console.log('Tiếp tục thực hiện các công việc khác.'); // Bước 4: In ra thông báo tiếp tục thực hiện các công việc khác
+
+//ở đoạn mã này chỉ ra 1 vd về hàm bất đồng bộ có dùng hàm setTimeout đề cho thời gian
+//chạy lâu hơn chạy lâu hơn nhằm cho các đoạn mã không cần kh có setTimeout đề sẽ chạy xong
+//xong và trả kết quả về khi nó chạy xong
