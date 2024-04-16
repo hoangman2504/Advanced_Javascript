74a7bf5e7848071596d5b6b68632bdeecb3f37e3
Javascript <doman.20042020@gmail.com>
2024-04-16 09:36:30 +0700

    Http Request Js

---
+++ #2.js
@@ -0,0 +1,17 @@
+var request = new XMLHttpRequest(); //1 dòng này để khai báo 1 lời gọi request
+
+//4
+request.onreadystatechange = function () {
+  if (this.readyState == 4) {
+    // Typical action to be performed when the document is ready:
+    console.log(">>> Check request", request);
+  }
+};
+//4 => đoạn mã này để lấy data về từ 1 request
+
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true); //2 dòng này để khởi tạo 1 request
+
+request.send(); //3 dòng này để chạy 1 request
+//note: trong bài code này đây cũng là ví dụ 1 asynchronous => bất đồng bộ khi các code không chạy theo
+//trình tự nhưng vẫn lấy data về khi truy cập đến 1 request
+//note: trình từ chạy code //1 => //2, 3, 4 v.v
