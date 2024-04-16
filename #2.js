4ca19d55e27eada4e952e2c1b52357b54e18b956
Javascript <doman.20042020@gmail.com>
2024-04-16 21:36:06 +0700

    Create http Request with Js

--- #2.js
+++ #2.js
@@ -1,3 +1,4 @@
+//!#2 HTTP Requests - Tạo Request Với JavaScript Thuần
 var request = new XMLHttpRequest(); //1 dòng này để khai báo 1 lời gọi request
 
 //4
@@ -15,3 +16,4 @@ request.send(); //3 dòng này để chạy 1 request
 //note: trong bài code này đây cũng là ví dụ 1 asynchronous => bất đồng bộ khi các code không chạy theo
 //trình tự nhưng vẫn lấy data về khi truy cập đến 1 request
 //note: trình từ chạy code //1 => //2, 3, 4 v.v
+
