e0139de84c2f7876ffaa84c9c0e4265df42735c5
Javascript <doman.20042020@gmail.com>
2024-04-17 09:22:49 +0700

    Status Request

--- #3.js
+++ #3.js
@@ -8,7 +8,7 @@ var request = new XMLHttpRequest(); //1 dòng này để khai báo 1 lời gọi
         console.log('>>> status', request.status);
     }
 };
-request.open("GET", "https://jsonplaceholder.typicode.com/todoss", true); //2 dòng này để khởi tạo 1 request
+request.open("TRACE", "https://jsonplaceholder.typicode.com/todoss", true); //2 dòng này để khởi tạo 1 request
 request.send(); //3 dòng này để chạy 1 request
 //ở đoạn code này giúp biết được status => trạng thái của của 1 request 200 ok => truy vấn và lấy data
 //từ 1 request về thành công, 404 => url không được đúng hay không được trình duyệt tìm thấy link
